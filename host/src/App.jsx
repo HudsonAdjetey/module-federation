58be89ec4bbff02d4cb8dc446f7d5449124279bd
Emmanuel Hudson <adjetey545@gmail.com>
2024-05-07 04:51:20 -0700

    host federation

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+
+import Button from "remoteApp/Button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,6 +13,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Button />
       </div>
     </>
   );
