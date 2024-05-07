791208d35bebb7f0e39f50d535bc00cea6282960
Emmanuel Hudson <adjetey545@gmail.com>
2024-05-07 04:56:24 -0700

    added shared button css

--- remote/src/Button.jsx
+++ remote/src/Button.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
+import "./Button.css";
 
 const Button = () => {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
+      <button
+        className="shared-btn"
+        onClick={() => setCount((prev) => prev + 1)}
+      >
+        {count}
+      </button>
     </div>
   );
 };
